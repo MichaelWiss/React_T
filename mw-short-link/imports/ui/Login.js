d181a798e2c48ec1a3c463c076e40fefab2b58fd
MichaelWiss <michael.wiss@gmail.com>
2017-07-11 18:04:52 -0500

    login

--- mw-short-link/imports/ui/Login.js
+++ mw-short-link/imports/ui/Login.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Login extends React.Component {
+	constructor(props) {
+       super(props);
+       this.state = {
+          error: ''
+       };
+	}
 	render() {
 		return (
 			<div>
-			   <h1>Login to Short Link</h1>
+			    <h1>Join Short Link</h1>
 
-			   Login form here
+			   {this.state.error ? <p>{this.state.error}</p> : undefined}
+               
+			   <form onSubmit={this.onSubmit.bind(this)}>
+	                <input type="email" ref="email" name="email" placeholder="Email"/>
+	                <input type="password" ref="password" name="password" placeholder="Password"/>
+	                <button>Create Account</button>
+                </form>
 
 			   <Link to="/signup">Have an account?</Link>
 			</div>
