74d304e65ffb7da9b056fb826d74bba95a44bd5c
MichaelWiss <michael.wiss@gmail.com>
2017-07-13 14:40:05 -0500

    error handling

--- mw-short-link/imports/ui/Signup.js
+++ mw-short-link/imports/ui/Signup.js
@@ -2,49 +2,42 @@ import React from 'react';
 import { Link } from 'react-router';
 import { Accounts } from 'meteor/accounts-base';
 
-
 export default class Signup extends React.Component {
-	constructor(props) {
-       super(props);
-       this.state = {
-          error: ''
-       };
-	}
-	onSubmit(e) {
-		e.preventDefault();
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    };
+  }
+  onSubmit(e) {
+    e.preventDefault();
 
-		let email = this.refs.email.value.trim();
-		let password = this.refs.password.value.trim();
-        
-		Accounts.createUser({email, password}, (err) => {
-           if (err) {
-              this.seState({error: err.reason });
-           } else {
-             this.setState({error: ''});
-           } 
-		});
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
 
-		// this.setState({
-		// 	error: 'Something went wrong.'
-		// });
-	}
-	
-	render() {
-		return (
-		 <div>
-			   <h1>Join Short Link</h1>
+    Accounts.createUser({email, password}, (err) => {
+      if (err) {
+        this.setState({error: err.reason});
+      } else {
+        this.setState({error: ''});
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Join Short Lnk</h1>
 
-			   {this.state.error ? <p>{this.state.error}</p> : undefined}
-               
-			   <form onSubmit={this.onSubmit.bind(this)}>
-	                <input type="email" ref="email" name="email" placeholder="Email"/>
-	                <input type="password" ref="password" name="password" placeholder="Password"/>
-	                <button>Create Account</button>
-                </form>
-			  
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-			 <Link to="/">Have an account?</Link>
-			</div>
-		); 
-	}
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" ref="email" name="email" placeholder="Email"/>
+          <input type="password" ref="password" name="password" placeholder="Password"/>
+          <button>Create Account</button>
+        </form>
+
+        <Link to="/">Already have an account?</Link>
+      </div>
+    );
+  }
 }
