e3bf20d1fd4a858f53bb5bd4182a34f4a35be1e7
MichaelWiss <michael.wiss@gmail.com>
2017-07-13 14:59:42 -0500

    login error handling

--- mw-short-link/imports/ui/Login.js
+++ mw-short-link/imports/ui/Login.js
@@ -16,9 +16,13 @@ export default class Login extends React.Component {
 		let password = this.refs.password.value.trim();
 
         Meteor.loginWithPassword({email}, password, (err) => {
-        	console.log('Login callback', err);
+        	if (err) {
+              this.setState({error: err.reason});
+            } else {
+              this.setState({error: ''});
+            }
         });
-	}
+	  }
 	render() {
 		return (
 			<div>
