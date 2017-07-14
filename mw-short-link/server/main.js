67dd47e8ca38eb2d38b15ea89f0e977d87f73bea
MichaelWiss <michael.wiss@gmail.com>
2017-07-14 09:27:22 -0500

    petSchema

--- mw-short-link/server/main.js
+++ mw-short-link/server/main.js
@@ -1,5 +1,23 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+
+  const petSchema = new SimpleSchema({
+  	name: {
+  		type: String,
+  		min: 1,
+  		max: 200
+  	},
+  	age: {
+  		type: Number,
+  		min: 0
+  	}
+ });
+
+  petSchema.validate({
+     name: 'Andrew'
+  });
 });
