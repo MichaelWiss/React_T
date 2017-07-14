6c2b2756d06cf85ede6cb7f8bf90aed622dbf58d
MichaelWiss <michael.wiss@gmail.com>
2017-07-14 12:56:28 -0500

    emplyee schema

--- mw-short-link/server/main.js
+++ mw-short-link/server/main.js
@@ -5,25 +5,39 @@ Meteor.startup(() => {
   // code to run on server at startup
 
 
-  const petSchema = new SimpleSchema({
-  	name: {
-  		type: String,
-  		min: 1,
-  		max: 200,
-  		optional: true
-  	},
-  	age: {
-  		type: Number,
-  		min: 0
-  	},
-  	contactNumber: {
-  		type: String,
-  		optional: true,
-  		regex: SimpleSchema.RegEx.Phone
-  	}
+  // const petSchema = new SimpleSchema({
+  // 	name: {
+  // 		type: String,
+  // 		min: 1,
+  // 		max: 200,
+  // 		optional: true
+  // 	},
+  // 	age: {
+  // 		type: Number,
+  // 		min: 0
+  // 	},
+  // 	contactNumber: {
+  // 		type: String,
+  // 		optional: true,
+  // 		regex: SimpleSchema.RegEx.Phone
+  // 	}
+  const employeeSchema = new SimpleSchema({
+       name : {
+       	  type: String,
+       	  min: 1,
+       	  max: 201,  
+       },
+       hourlyWage: {
+       	  type: Number,
+       	  min: 0
+       },
+       email: {
+       	regEx: SimpleSchema.RegEx.Email
+       }
  });
 
   petSchema.validate({
      age: 2,
+     contactNumber: '1234'
   });
 });
