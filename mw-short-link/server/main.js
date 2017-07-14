cc683881b87db8a971454789b00ab4d6fe4e2999
MichaelWiss <michael.wiss@gmail.com>
2017-07-14 13:06:27 -0500

    account validation

--- mw-short-link/server/main.js
+++ mw-short-link/server/main.js
@@ -1,8 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Accounts.validateNewUser(() => {
+
+  });
 
 
   // const petSchema = new SimpleSchema({
