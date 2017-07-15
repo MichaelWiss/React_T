20187846e88a0bb3e764dbfcf672f37d864799fe
MichaelWiss <michael.wiss@gmail.com>
2017-07-15 14:36:48 -0500

    correct email error message

--- mw-short-link/server/main.js
+++ mw-short-link/server/main.js
@@ -10,21 +10,22 @@ Meteor.startup(() => {
   Accounts.validateNewUser((user) => {
   	 const email = user.emails[0].address;
       
-     new SimpleSchema({
+    try {
+       new SimpleSchema({
      	email: {
            type: String,
            regEx: SimpleSchema.RegEx.Email
      	}
   	}).validate({ email })
 
+    } catch (e) {
+      throw new Meteor.Error(400, e.message)
+    }
+
      return true;
   });
 
-  try {
-    throw new Meteor.Error(400, 'Please enter a valid email');
-  } catch (e) {
-    console.log(e);
-  }
+
 
 
   // const petSchema = new SimpleSchema({
