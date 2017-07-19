2ffa236388eb17960e47d9a821a945c11a8c2fde
MichaelWiss <michael.wiss@gmail.com>
2017-07-19 18:56:59 -0500

    component will unmount linkslist

--- mw-short-link/imports/ui/LinksList.js
+++ mw-short-link/imports/ui/LinksList.js
@@ -4,6 +4,9 @@ export default class LinksList extends React.Component {
 	componentDidMount() {
 		console.log('componentDidMount LinksList');
 	}
+	componentWillUnmount() {
+		console.log('componentWillUnmount LinksList');
+	}
 	render() {
 		return (
           <div>
