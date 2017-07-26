c77c903c4f146bb4dc7564f3e7113bc289d917ec
MichaelWiss <michael.wiss@gmail.com>
2017-07-26 10:12:14 -0500

    simple-schema-configuration

--- mw-short-link/imports/api/links.js
+++ mw-short-link/imports/api/links.js
@@ -22,6 +22,7 @@ Meteor.methods({
       new SimpleSchema({
      	url: {
            type: String,
+           label: 'Your link',
            regEx: SimpleSchema.RegEx.Url
      	}
   	}).validate({ url })
---
+++ mw-short-link/imports/startup/simple-schema-configuration.js
@@ -0,0 +1,3 @@
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+
