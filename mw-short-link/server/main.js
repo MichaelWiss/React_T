9f23787e5dff0d17a81d708544d5959de64b89b8
MichaelWiss <michael.wiss@gmail.com>
2017-07-30 13:32:22 -0500

    clean up

--- mw-short-link/server/main.js
+++ mw-short-link/server/main.js
@@ -21,27 +21,27 @@ Meteor.startup(() => {
     	next()
      }
     });
-    // //set http status code to 302
-    // res.statusCode = 302;
-    // //set location header to 'http://www.google.com'
-    // res.setHeader('Location', 'http://www.google.com')
+ //    // //set http status code to 302
+ //    // res.statusCode = 302;
+ //    // //set location header to 'http://www.google.com'
+ //    // res.setHeader('Location', 'http://www.google.com')
 
-    // res.write('<h1>This is my middleware</h1>');
-    // //end the request
-    // res.end();
+ //    // res.write('<h1>This is my middleware</h1>');
+ //    // //end the request
+ //    // res.end();
 
-	WebApp.connectHandlers.use((req, res, next) => {
-      console.log('This is my custom middleware');
-      console.log(req.url, req.method, req.headers, req.query);
- //      //set HTTP status code
- //      res.statusCode = 404;
- //      //set HTTP headers
- //      res.setHeader('my-custom-success-header', 'Michael was here')
- //      //set HTTP body
- //      // res.write('<h1>This is my middleware</h1>');
- //      //end HTTP request
- //      res.end();
+	// WebApp.connectHandlers.use((req, res, next) => {
+ //      console.log('This is my custom middleware');
+ //      console.log(req.url, req.method, req.headers, req.query);
+ // //      //set HTTP status code
+ // //      res.statusCode = 404;
+ // //      //set HTTP headers
+ // //      res.setHeader('my-custom-success-header', 'Michael was here')
+ // //      //set HTTP body.   
+ // //      // res.write('<h1>This is my middleware</h1>');
+ // //      //end HTTP request
+ // //      res.end();
 
-      next();
-	});
+ //      next();
+	// });
 });
