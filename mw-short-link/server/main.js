2b4fc6858a3560fab9cec95aa8d049796a931b0e
MichaelWiss <michael.wiss@gmail.com>
2017-07-30 11:41:06 -0500

    302 redirect to google

--- mw-short-link/server/main.js
+++ mw-short-link/server/main.js
@@ -7,18 +7,29 @@ import '../imports/startup/simple-schema-configuration.js';
 
 
 Meteor.startup(() => {
-	WebApp.connectHandlers.use((req, res, next) => {
-      console.log('This is my custom middleware');
-      console.log(req.url, req.method, req.headers, req.query);
-      //set HTTP status code
-      res.statusCode = 404;
-      //set HTTP headers
-      res.setHeader('my-custom-success-header', 'Michael was here')
-      //set HTTP body
-      // res.write('<h1>This is my middleware</h1>');
-      //end HTTP request
-      res.end();
-      
-      next();
+    //creating and registering new middleware function
+    WebApp.connectHandlers.use((req, res, next) => {
+    //set http status code to 302
+    res.statusCode = 302;
+    //set location header to 'http://www.google.com'
+    res.setHeader('Location', 'http://www.google.com')
+
+    res.write('<h1>This is my middleware</h1>');
+    //end the request
+    res.end();
+
+	// WebApp.connectHandlers.use((req, res, next) => {
+ //      console.log('This is my custom middleware');
+ //      console.log(req.url, req.method, req.headers, req.query);
+ //      //set HTTP status code
+ //      res.statusCode = 404;
+ //      //set HTTP headers
+ //      res.setHeader('my-custom-success-header', 'Michael was here')
+ //      //set HTTP body
+ //      // res.write('<h1>This is my middleware</h1>');
+ //      //end HTTP request
+ //      res.end();
+
+ //      next();
 	});
 });
