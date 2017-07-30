ba5e647fcd4950fdcf635105ea7686ae02b73e66
MichaelWiss <michael.wiss@gmail.com>
2017-07-30 13:25:42 -0500

    find link, then redirect

--- mw-short-link/server/main.js
+++ mw-short-link/server/main.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users';
-import '../imports/api/links';
+import { Links } from '../imports/api/links';
 import '../imports/startup/simple-schema-configuration.js';
 
 
@@ -10,6 +10,17 @@ Meteor.startup(() => {
     //creating and registering new middleware function
     WebApp.connectHandlers.use((req, res, next) => {
     const _id = req.url.slice(1);
+
+    const link = Links.findOne({ _id });
+
+    if (link) {
+       res.statusCode = 302;
+       res.setHeader('Location', link.url);
+       res.end();
+    } else {
+    	next()
+     }
+    });
     // //set http status code to 302
     // res.statusCode = 302;
     // //set location header to 'http://www.google.com'
